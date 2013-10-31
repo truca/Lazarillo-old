19e931dfd4b16d43774d147b62bb60999ba9ed1f
Ignacio Ureta Carmona <trucoso@gmail.com>
2013-10-30 21:21:24 -0300

    init

---
+++ app2/upload.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+
+	</head>
+	<body>
+		<h1>Formulario de subida para mapa</h1><br>
+		<form method="POST" action="selectPoints.php" enctype="multipart/form-data">
+			Imagen: <input type="file" name="file" id="file"/><br><br>
+			<input type="submit" value="Subir Imagen"/>
+		</form>
+	</body>
+</html>
