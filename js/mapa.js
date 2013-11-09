0f8f24c1654191fc35ca0d1c097e5934a5640b32
Ignacio Ureta Carmona <trucoso@gmail.com>
2013-11-09 16:14:28 -0300

    adaptacion tamaño de interfaz y ordenamiento de la carpeta

--- js/mapa.js
+++ js/mapa.js
@@ -57,7 +57,7 @@ function rapha()
     foto.src="http://www.lazarillo.cl/hito1/upload/images/uni.png"; //recordar que tienes que obtener el mapa cuando se edita de esta forma, te tiene que llegar como parametro
     var anchoimg=foto.width;
     var altoimg=foto.height;
-    var caja = Raphael('wrap','100%', '100%');
+    var caja = Raphael('wrap','100%', '90%');
     //var botones_mapa = Raphael('wrap', '5%', '20%');
 
     caja.setViewBox(1,1,anchoimg,altoimg, true);
